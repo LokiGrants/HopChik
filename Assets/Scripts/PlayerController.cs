30089baff6567d9bfa6bb588fc5fda0924903576
Pedro Loures <pedroh.z.loures@gmail.com>
2021-04-01 15:05:10 -0400

    Chik left distress because has animations!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,52 +2,148 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(CharacterController))]
-
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
-    public CharacterController controller;
-    public float jumpHeight = 3.0f;
-    public float playerSpeed = 2.0f;
-    public float gravityValue = -9.81f;
+    public CharacterController playerControl;
 
-    private Vector3 playerVelocity;
-    private Vector3 lastPosition;
-    private bool groundedPlayer;
+    public float actPlayerSpeed;
+    //public float playerSprint;
+    //public float playerDuck;
+    public float playerJumpForce;
+    //public float playerRotateSpeed;
 
-    void Update()
+    //private Vector3 playerMotionInput;
+    private Vector3 playerMotionVector;
+    private Animator playerAnimator;
+    private Coroutine rotateAround;
+    private bool isFacingLeft;
+    private bool jumping = false;
+    private float timeForNextIdle;
+
+    private void Start()
     {
-        Debug.Log(groundedPlayer);
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        timeForNextIdle = Time.timeSinceLevelLoad + Random.Range(30, 60) / 10f;
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void GetInput()
+    {
+        playerMotionVector = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+
+        if (Input.GetAxis("Horizontal") > 0)
         {
-            playerVelocity.y = 0f;
+            if (isFacingLeft)
+            {
+                isFacingLeft = false;
+                StartCoroutine(LerpFunction(Quaternion.Euler(new Vector3(0, 90, 0)), .25f));
+            }
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            if (!isFacingLeft)
+            {
+                isFacingLeft = true;
+                StartCoroutine(LerpFunction(Quaternion.Euler(new Vector3(0, -90, 0)), .25f));
+            }
         }
 
-        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
+        //Get input from WASD keys.
+        //playerMotionInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //playerMotionVector = transform.TransformDirection(playerMotionInput);
+
+
+        //LeftCtrl to sprint. LeftShift to crouch.
+        /*if (Input.GetAxis("Sprint") != 0 && playerControl.isGrounded)
         {
-            transform.forward = move;
+            actPlayerSpeed = playerSprint;
         }
-
-        // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        else
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * gravityValue);
-            animator.SetTrigger("Jump");
+            actPlayerSpeed = playerSpeed;
+        }*/
+        //if (Input.GetAxis("Duck") != 0)
+        //{
+        //    actPlayerSpeed = playerDuck;
+        //}
+        //if (Input.GetAxis("Sprint") == 0 && Input.GetAxis("Duck") == 0)
+        //{
+        //    actPlayerSpeed = playerSpeed;
+        //}
+    }
+
+    private void Movement()
+    {
+        playerAnimator.SetBool("Walk", false);
+        if (jumping)
+        {
+            playerControl.Move(new Vector3(playerMotionVector.x * (actPlayerSpeed / 2.5f) * Time.deltaTime, playerJumpForce, playerMotionVector.z * (actPlayerSpeed / 2) * Time.deltaTime) * Time.deltaTime);
+            playerControl.SimpleMove(Vector3.zero * actPlayerSpeed * Time.deltaTime);
         }
+        else
+        {
+            playerControl.SimpleMove(playerMotionVector * actPlayerSpeed * Time.deltaTime);
+            if (playerMotionVector.magnitude > 0)
+            {
+                playerAnimator.SetBool("Walk", true);
+                timeForNextIdle += Time.fixedDeltaTime;
+            } else
+            {
+                if (Time.timeSinceLevelLoad > timeForNextIdle)
+                {
+                    int zeroToNine = Random.Range(0, 10000000) / 1000000;
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+                    if (zeroToNine > 6) //0 to 9
+                    {
+                        playerAnimator.SetBool("Turn Head", true);
+                        this.Invoke(() => DisableAnimation("Turn Head"), 1f);
+                    }
+                    else if (zeroToNine > 3)
+                    {
+                        playerAnimator.SetBool("Eat", true);
+                        this.Invoke(() => DisableAnimation("Eat"), 1f);
+                    }
 
-        // Speed is the distance between my current position - last position over time
-        float speed = Vector3.Distance(lastPosition, transform.position) / Time.deltaTime;
-        animator.SetFloat("Speed", speed);
-        lastPosition = this.transform.position;
+                    timeForNextIdle = Time.timeSinceLevelLoad + Random.Range(30,60)/10f;
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxis("Jump") != 0 && playerControl.isGrounded)
+        {
+            jumping = true;
+            playerAnimator.SetBool("Jump", jumping);
+        }
+        else if (playerControl.isGrounded)
+        {
+            if (jumping)
+            {
+                jumping = false;
+                playerAnimator.SetBool("Jump", jumping);
+            }
+        }
+        GetInput();
+        Movement();
+    }
+
+    void DisableAnimation(string boolName)
+    {
+        playerAnimator.SetBool(boolName, false);
+    }
+
+    IEnumerator LerpFunction(Quaternion endValue, float duration)
+    {
+        float time = 0;
+        Quaternion startValue = transform.rotation;
+
+        while (time < duration)
+        {
+            transform.rotation = Quaternion.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = endValue;
     }
 }
