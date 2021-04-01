73b97f0585fd007a8f259a41ebe2c299025868a9
Olena Hrynkiw <hrynkiw.olena@gmail.com>
2021-04-01 17:21:48 -0400

    SceneLoader + UI
    Added Sceneloader script and functionality. Added some UI to main menu and main game scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// this fn will load a different scene when it is called. the # refers to the index of the scene.
+    /// create an idex on unity through File>Build settings>add open scenes (must have desired scene open to add)
+    /// - when a start game button is pressed, OnLoadSceneCalled() must be called.
+    /// Add an eveent to button to make it happen
+    /// Note: you must also add the script to the button or another game object
+    /// </summary>
+    public void OnLoadSceneCalled()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnLoadSceneCalled(int sceneNum)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+
+}
