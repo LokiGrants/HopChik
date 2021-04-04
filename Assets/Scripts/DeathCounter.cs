cd9ba745eac8083096acb02e73b7232077ad15f0
Pedro Loures <pedroh.z.loures@gmail.com>
2021-04-04 14:36:32 -0400

    oopsie

---
+++ Assets/Scripts/DeathCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCounter : Singleton<DeathCounter>
+{
+    [HideInInspector]
+    public int deathCounter;
+
+    public void DeadCounter()
+    {
+        deathCounter++;
+    }
+}
---
+++ Assets/Scripts/DeathTextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DeathTextController : MonoBehaviour
+{
+    private TextMeshProUGUI deathText;
+
+    private void Start()
+    {
+        deathText = GetComponent<TextMeshProUGUI>();
+        deathText.text = "Deaths in this session: " + DeathCounter.Instance.deathCounter;
+    }
+
+    private void OnEnable()
+    {
+        if (deathText == null)
+            deathText = GetComponent<TextMeshProUGUI>();
+        deathText.text = "Deaths in this session: " + DeathCounter.Instance.deathCounter;
+    }
+}
