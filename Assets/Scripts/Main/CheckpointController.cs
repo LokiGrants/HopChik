05c74ed505154636d4d3b53e294379f55726e692
Pedro Loures <pedroh.z.loures@gmail.com>
2021-04-08 15:33:13 -0400

    OOPS

---
+++ Assets/Scripts/Main/CheckpointController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    public float checkpointMoveWaitTime = .5f;
+
+    private Coroutine arrivedAtCheckpoint;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (arrivedAtCheckpoint == null)
+        {
+            arrivedAtCheckpoint = StartCoroutine(ArrivedAtCheckpoint());
+        }
+    }
+
+    IEnumerator ArrivedAtCheckpoint()
+    {
+        ObjectPoolingManager.Instance.SwitchCheckpoint();
+        //Particles to start for movement or animation
+        yield return new WaitForSeconds(checkpointMoveWaitTime);
+        ObjectPoolingManager.Instance.MoveCheckpoint();
+
+        arrivedAtCheckpoint = null;
+    }
+}
---
+++ Assets/Scripts/Main/ObjectPoolingManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPoolingManager : Singleton<ObjectPoolingManager>
+{
+    public Transform checkpointObject;
+    public List<Transform> checkpointPositions;
+
+    public Material checkpointMaterial, checkpointMaterialShadow;
+    public List<Texture> allPossibleFoods;
+
+    private int listIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SwitchCheckpoint();
+        MoveCheckpoint();
+    }
+
+    public void SwitchCheckpoint()
+    {
+        PlayerController.Instance.ChangeCheckpoint(checkpointPositions[listIndex]);
+    }
+
+    public void MoveCheckpoint()
+    {
+        listIndex++;
+        if (listIndex < checkpointPositions.Count-1)
+        {
+            checkpointObject.position = checkpointPositions[listIndex].position;
+            RandomizeFruit();
+        } else
+        {
+            checkpointObject.gameObject.SetActive(false);
+        }
+    }
+
+    public void RestartLevel()
+    {
+        listIndex = 0;
+        checkpointObject.gameObject.SetActive(true);
+        SwitchCheckpoint();
+        MoveCheckpoint();
+        ReviveLevel();
+    }
+
+    public void ReviveLevel()
+    {
+        PlayerController.Instance.Respawn();
+    }
+
+    public void RandomizeFruit()
+    {
+        Texture chosenFood = allPossibleFoods[Random.Range(0, allPossibleFoods.Count)];
+
+        checkpointMaterial.mainTexture = chosenFood;
+        checkpointMaterialShadow.mainTexture = chosenFood;
+    }
+}
