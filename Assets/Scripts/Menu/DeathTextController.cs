a9552ab9bff9d5074efbe15c64cb47966427312b
Olena Hrynkiw <hrynkiw.olena@gmail.com>
2021-04-15 16:03:08 -0400

    Death Counter Update
    Changed the string result and added background image for death

--- Assets/Scripts/Menu/DeathTextController.cs
+++ Assets/Scripts/Menu/DeathTextController.cs
@@ -10,13 +10,13 @@ public class DeathTextController : MonoBehaviour
     private void Start()
     {
         deathText = GetComponent<TextMeshProUGUI>();
-        deathText.text = "Deaths in this session: " + GameFlags.Instance.deathCounter;
+        deathText.text = GameFlags.Instance.deathCounter.ToString();
     }
 
     private void OnEnable()
     {
         if (deathText == null)
             deathText = GetComponent<TextMeshProUGUI>();
-        deathText.text = "Deaths in this session: " + GameFlags.Instance.deathCounter;
+        deathText.text = GameFlags.Instance.deathCounter.ToString();
     }
 }
